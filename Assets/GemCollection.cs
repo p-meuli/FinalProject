881cce5a7dfcdfb4a4be18558ddc53a5b2978de4
p-meuli <pmeuli@colostate.edu>
2025-05-13 02:44:06 -0600

    redid Gems
    had to revert and start over, so redid gems

--- Assets/GemCollection.cs
+++ Assets/GemCollection.cs
@@ -5,15 +5,20 @@ public class GemCollection : MonoBehaviour
     private int gem = 0;
 
     public TextMeshProUGUI GemText;
+    public TextMeshProUGUI EndText;
 
     private void OnTriggerEnter(Collider other)
     {
     if(other.transform.tag == "stone")
     {
         gem += other.GetComponent<gemValue>().Value;
-        GemText.text = "Gems needed: " + gem.ToString() + "/10";
+        GemText.text = "Gems needed: " + gem.ToString() + "/70,000";
         Debug.Log(gem);
         Destroy(other.gameObject);
     }
+    if (gem >= 70000)
+    {
+    EndText.text = "Ewe're Winner!!!!1!!";
     }
+ }
 }
