40c7e21ab88f602a8589f834f18c7a7ac4d29157
p-meuli <pmeuli@colostate.edu>
2025-05-13 20:11:50 -0600

    fixed build

--- Assets/ShootSpore.cs
+++ Assets/ShootSpore.cs
@@ -1,5 +1,4 @@
 using System.Linq;
-using JetBrains.Rider.Unity.Editor;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -9,13 +8,7 @@ public class ShootSpore : MonoBehaviour
     public Transform FirePoint;
     void Update()
     {
-        /*
-        if(Input.GetKeyDown(KeyCode.J)) 
-        {
-            GameObject bullet = Instantiate(BulletPrefab, FirePoint.position, Quaternion.identity);
-            bullet.GetComponent<Rigidbody>().AddForce(transform.right * 1000);
-        }
-*/
+       
         if(Input.GetKeyDown(KeyCode.J)) 
         {
             GameObject bullet = Instantiate(BulletPrefab, FirePoint.position, Quaternion.identity);
@@ -23,45 +16,5 @@ public class ShootSpore : MonoBehaviour
                 
         }
        
-       /*
-         if(Input.GetKeyDown(KeyCode.J)) 
-         {
-            GameObject bullet = Instantiate(BulletPrefab, FirePoint.position, Quaternion.identity);
-             bullet.GetComponent<Rigidbody>().AddForce(transform.up * 1000);
-         }
-        */
-        
-
-
     }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-/*{
-
-public GameObject BulletPrefab; 
-
-void Update()
-{
-    if(Input.GetKeyDown(KeyCode.J)) //FirePosition
-    {
-        GameObject bullet = Instantiate(BulletPrefab, FirePoint.position, Quaternion.identity);
-        bullet.GetComponent<Rigidbody>().AddForce(transform.right = 1000f);
-    }
-    
-
-// Spore prefab
-// FirePosition gameObject
-}
-}
-*/
--- Assets/SporeBullet.cs
+++ Assets/SporeBullet.cs
@@ -1,31 +1,15 @@
-using UnityEditor.Rendering;
 using UnityEngine;
 
 public class SporeBullet : MonoBehaviour
 {
- /*
-    void Start()
-    {
-        // set spore with spore tag
-        gameObject.tag = "Spore";
-    }
-    */
+
     private void OnCollisionEnter(Collision collision)
     {
-        //Destroy(gameObject);   
-        //Enemys to take damage
 
         if(collision.gameObject.TryGetComponent<Enemy2>(out Enemy2 enemyComponent)) 
         {
             enemyComponent.TakeDamage(5);
         }
 
-       /*
-        if(Collision.tag = "Enemy")
-        {
-
-        }
-        if(collision.tag = "boomer") 
-        */
     }
 }
